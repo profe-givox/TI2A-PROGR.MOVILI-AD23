257f3efd4cbc83d77a25cd4b94f578f465cb721d
Gustavo Ivan Vega Olvera <gi.vega@itsur.edu.mx>
2023-08-30 15:59:08 -0600

    Agregar el componente Image al proyecto dice roller

--- DiceRoller/app/src/main/java/net/ivanvega/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/net/ivanvega/diceroller/MainActivity.kt
@@ -3,13 +3,23 @@ package net.ivanvega.diceroller
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import net.ivanvega.diceroller.ui.theme.DiceRollerTheme
 
 class MainActivity : ComponentActivity() {
@@ -28,6 +38,23 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun DiceWithButtonAndImage(modifier: Modifier = Modifier){
 
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(painter = painterResource(id = R.drawable.dice_1) ,
+
+            contentDescription = stringResource(
+            R.string.imagen_de_dado
+            )
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = { /*TODO*/ }) {
+            Text(stringResource(R.string.roll))
+        }
+    }
+
+
 }
 
 @Preview
@@ -36,6 +63,7 @@ fun DiceRollerApp(){
     DiceWithButtonAndImage()
 }
 
+/*
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
         Text(
@@ -50,4 +78,4 @@ fun GreetingPreview() {
     DiceRollerTheme {
         Greeting("Android desde Moroleon")
     }
-}
+}*/
