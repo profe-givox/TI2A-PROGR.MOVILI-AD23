139da56df432814cffdccb882882dca8ce42fe55
Gustavo Ivan Vega Olvera <gi.vega@itsur.edu.mx>
2023-11-13 15:56:35 -0600

    Proyecto: Inventario Tema: Bases de datos con Room Avance: Ya lista productos agregados

--- basic-android-kotlin-compose-training-inventory-app-starter/app/src/main/java/com/example/inventory/ui/AppViewModelProvider.kt
+++ basic-android-kotlin-compose-training-inventory-app-starter/app/src/main/java/com/example/inventory/ui/AppViewModelProvider.kt
@@ -54,7 +54,7 @@ object AppViewModelProvider {
 
         // Initializer for HomeViewModel
         initializer {
-            HomeViewModel()
+            HomeViewModel(inventoryApplication().container.itemsRepository)
         }
     }
 }
